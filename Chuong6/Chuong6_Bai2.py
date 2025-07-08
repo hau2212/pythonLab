d2f4105b85c50818b66331f1f2e1a98a2915982a
thanh hau <mr.hau221222@gmail.com>
2025-07-08 23:22:09 +0700

    chuong_moi_testgit1

---
+++ Chuong6/Chuong6_Bai2.py
@@ -0,0 +1,26 @@
+"""
+nguyen thanh hau
+24-0-00627
+chuong 6 bai 2
+chuong trinh nguoi dung nhap ten file va hien thi 5 dong
+"""
+import sys
+sys.path.append("..")
+from resource import res as e
+import os
+
+def hienThi(fileName):
+    try:
+        base_path = os.path.dirname(__file__)
+        full_path = os.path.join(base_path, "Ch6", fileName)
+
+        with open(full_path, "r") as file:
+            for _ in range(5):
+                line = file.readline()
+                if not line:
+                    break
+                print(line.strip())
+    except Exception as e:
+        print("Lỗi:", e)
+
+hienThi("numbers.txt")
